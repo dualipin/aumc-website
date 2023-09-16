be3f0d628c99bada4f8a95b92ad2cef32952b035
dualipin <118947989+dualipin@users.noreply.github.com>
2023-09-16 16:20:15 -0600

    update style and features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
+import { RouterOutlet } from "@angular/router"
+import * as Aos from 'aos'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    Aos.init()
+  }
+  title = "page-aumc"
+
   onDeactivate() {
-    window.scrollTo(0, 0);
+    window.scrollTo(0, 0)
     // Alternatively, you can scroll to top by using this other call:
     // window.scrollTo(0, 0)
+  }
+
+  prepareRoute(outlet: RouterOutlet) {
+    return (
+      outlet &&
+      outlet.activatedRouteData &&
+      outlet.activatedRouteData["animation"]
+    )
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {CommonModule} from '@angular/common';
-import { SpinnersAngularModule } from 'spinners-angular';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { NosotrosComponent } from './components/nosotros/nosotros.component';
-import { ContactoComponent } from './components/contacto/contacto.component';
+import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ServiciosComponent } from './components/servicios/servicios.component';
-import { CalculadoraComponent } from './components/calculadora/calculadora.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './pages/not-found/page-not-found.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    NosotrosComponent,
-    ContactoComponent,
     FooterComponent,
-    ServiciosComponent,
-    CalculadoraComponent,
     PageNotFoundComponent,
   ],
   imports: [
@@ -30,14 +22,13 @@ import { RouterModule } from '@angular/router';
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
-      { path: 'contacto', component: ContactoComponent },
-      { path: 'servicios', component: ServiciosComponent },
-      { path: 'nosotros', component: NosotrosComponent },
-      { path: 'nav', component: NavbarComponent },
+      { path: 'contacto', loadComponent: () => import('./pages/contacto/contacto.component').then(c => c.ContactoComponent) },
+      { path: 'servicios', loadComponent: () => import('./pages/servicios/servicios.component').then(c => c.ServiciosComponent) },
+      { path: 'nosotros', loadComponent: () => import('./pages/nosotros/nosotros.component').then(c => c.NosotrosComponent) },
       { path: '**', component: PageNotFoundComponent },
     ]),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
