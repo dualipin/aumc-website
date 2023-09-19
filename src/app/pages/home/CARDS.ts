642477c85eb32ae69b1a899bb382eea6d7e3ae60
dualipin <118947989+dualipin@users.noreply.github.com>
2023-09-18 23:10:36 -0600

    add google map

---
+++ src/app/pages/home/CARDS.ts
@@ -0,0 +1,43 @@
+import { ICard } from "app/interfaces/ICard";
+
+export const CARDS_VENTAJAS: ICard[] = [
+  {
+    title: "Te ahorra tiempo y dinero",
+    description: "Contratar a un Contador Profesional hará que ganes tiempo para enfocarte en lo que verdaderamente importa, el crecimiento de tu empresa.",
+    icon: 'bi bi-clock'
+  },
+  {
+    title: 'Te ayuda a tomar decisiones',
+    description: 'Los pronósticos de ingresos te ayudan a comprender si las estrategias comerciales actuales están funcionando y generando resultados o si es necesario realizar algunos cambios.',
+    icon: 'bi bi-bar-chart'
+  },
+  {
+    title: 'Flujo de caja',
+    description: 'La gestión y planificación con respecto a tu flujo de efectivo es lo que mantendrá tu negocio abierto durante los primeros meses.',
+    icon: 'bi bi-cash-stack'
+  },
+  {
+    title: 'Planificacion y control',
+    description: 'La contabilidad proporciona información que apoya el proceso de toma de decisiones en el negocio, en relación con la planificación y el control, haciendo más eficientes los procesos de la empresa.',
+    icon: 'bi bi-clipboard-check'
+  }
+];
+
+export const CARDS_SERVICIOS: ICard[] = [
+  {
+    title: "Facturación",
+    description: 'Te ofrecemos servicios para facturar gastos de operaciones, facturación digital y facturación de campo'
+  },
+  {
+    title: 'Declaraciones',
+    description: 'Te ofrecemos servicios para declaración de impuestos, gastos y impuestos atrasados'
+  },
+  {
+    title: 'Calculo de impuestos',
+    description: 'Te ofrecemos servicios para calculo de impuestos fiscales, ISSR, Seguro social.',
+  },
+  {
+    title: 'Contabilidad',
+    description: 'Te ofrecemos servicios para contabilidad electrónica, contabilidad de costos, contabilidad de activos fijos, contabilidad de nómina, contabilidad de inventarios, contabilidad de bancos, contabilidad de clientes y proveedores, contabilidad de gastos, contabilidad de ingresos, contabilidad de depreciación'
+  }
+]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CARDS_SERVICIOS, CARDS_VENTAJAS } from './CARDS';
+import { ICard } from 'app/interfaces/ICard';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  cardsVentajas: ICard[] = CARDS_VENTAJAS;
+  cardsServicios: ICard[] = CARDS_SERVICIOS;
+
+
   constructor() { }
+
 
   ngOnInit(): void {
   }
