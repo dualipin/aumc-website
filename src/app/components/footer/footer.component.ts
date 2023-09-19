375779edda84b21b869972bdc6bd209fba4ec618
dualipin <118947989+dualipin@users.noreply.github.com>
2023-09-18 23:13:59 -0600

    update style and remove unuse links

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface SocialNetwork {
+  name: string;
+  url: string;
+  icon?: string;
+}
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  socialNetworks: SocialNetwork[] = [
+    {
+      name: 'facebook',
+      url: 'https://www.facebook.com/aumc.services',
+      icon: 'bi bi-facebook'
+    },
+    {
+      name: 'instagram',
+      url: 'https://www.instagram.com/aumcservices.oficial/',
+      icon: 'bi bi-instagram'
+    },
+    {
+      name: 'whatsapp',
+      url: 'https://api.whatsapp.com/send?phone=+522284208734&text=Hola,%20me%20gustar%C3%ADa%20saber%20m%C3%A1s%20sobre%20sus%20servicios',
+      icon: 'bi bi-whatsapp'
+    },
+  ]
 
 }
