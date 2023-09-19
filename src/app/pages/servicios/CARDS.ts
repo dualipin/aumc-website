1858161e0202fb14878bd1a5d0ecb046fa78fb4e
dualipin <118947989+dualipin@users.noreply.github.com>
2023-09-18 23:11:34 -0600

    update style (responsive and themes)

---
+++ src/app/pages/servicios/CARDS.ts
@@ -0,0 +1,76 @@
+import { ICard } from "app/interfaces/ICard";
+
+export const CARDS_SERVICIOS: ICard[] = [
+    {
+        title: 'Calculo de impuestos',
+        description: 'Te ofrecemos servicios para calculo de impuestos fiscales, ISSR, Seguro social.'
+    },
+    {
+        title: 'Facturación',
+        description: 'Te ofrecemos servicios para facturar gastos de operaciones, facturación digital y facturación de campo',
+    },
+    {
+        title: 'Declaraciones',
+        description: 'Te ofrecemos servicios para declaración de impuestos, gastos y impuestos atrasados'
+    },
+    {
+        title: 'Cálculo de impuestos',
+        description: 'Te ofrecemos servicios para calculo de impuestos fiscales, ISSR, Seguro social.'
+    },
+    {
+        title: 'Citas SAT',
+        description: 'Olvídate de estar batallando en conseguir una cita en el SAT, nosotros nos encargamos'
+    },
+    {
+        title: 'Cambio de domicilio fiscal',
+        description: 'Te ofrecemos servicios para cambio de domicilio fiscal, cambio de régimen fiscal, cambio de actividad económica'
+    },
+    {
+        title: 'Cédula de empadronamiento',
+        description: 'Te ofrecemos servicios para cédula de empadronamiento, cédula de identificación fiscal, cédula de identificación fiscal de personas físicas'
+    },
+    {
+        title: 'Saldo a favor',
+        description: 'Te ofrecemos servicios para saldo a favor, devolución de impuestos, devolución de impuestos atrasados'
+    },
+    {
+        title: 'Cambio de actividad económica',
+        description: 'Te ofrecemos servicios para cambio de actividad económica, cambio de régimen fiscal, cambio de domicilio fiscal'
+    },
+    {
+        title: 'Reclutamiento de personal',
+        description: '¿Tu negocio está en crecimiento?, nosotros te brindamos la tranquilidad de tener un personal adecuado a las necesidades de tu negocio'
+    },
+    {
+        title: 'Renovación de firma electrónica',
+        description: 'Te ofrecemos servicios para renovación de firma electrónica, renovación de contraseña, renovación de contraseña de firma electrónica'
+    },
+    {
+        title: 'Tramite de SAS',
+        description: '¿Necesitas una persona moral?, es un paso importante, una Sociedad por Acciones Simplificada puede ser una excelente opción para micro empresas y empresas pequeñas'
+    },
+    {
+        title: 'Constancia de situación fiscal',
+        description: 'Te brindamos tu constancia de situación fiscal para cualquier tramite que necesites'
+    },
+    {
+        title: 'Generación de sello digital',
+        description: '¡No te quedes sin la posibilidad de emitir facturas!, llámanos'
+    },
+    {
+        title: 'Alta y baja IMSS como patrón',
+        description: 'Si tienes empleados, es importante que estés al corriente con el IMSS, nosotros te ayudamos'
+    },
+    {
+        title: 'Alta y baja IMSS como trabajador',
+        description: 'Nos encargamos cuando tengas nuevo personal y se tenga que dar del baja en el seguro social'
+    },
+    {
+        title: 'Alta al padrón de contratistas (SEFIPLAN)',
+        description: 'Brinda servicios de obra pública y servicios relacionados con la misma'
+    },
+    {
+        title: 'Alta al padrón de proveedores (SEFIPLAN)',
+        description: 'Brinda servicios de obra pública y servicios relacionados con la misma'
+    }
+]
--- src/app/pages/servicios/servicios.component.ts
+++ src/app/pages/servicios/servicios.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ICard } from 'app/interfaces/ICard';
+import { CARDS_SERVICIOS } from './CARDS';
+import { CommonModule } from '@angular/common';
+
+
 @Component({
   selector: 'app-servicios',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './servicios.component.html',
-  styleUrls: ['./servicios.component.css']
 })
 export class ServiciosComponent implements OnInit {
+
+  //referencia a la interfaz ICard y a la constante CARDS_SERVICIOS
+  cards: ICard[] = CARDS_SERVICIOS;
 
   constructor() { }
 
