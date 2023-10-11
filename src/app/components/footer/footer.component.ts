009ffae9906bc6b60d362cf16f7ca0abc7b580f0
dualipin <118947989+dualipin@users.noreply.github.com>
2023-10-10 21:44:38 -0600

    add link email

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -11,23 +11,26 @@ interface SocialNetwork {
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
-
   socialNetworks: SocialNetwork[] = [
     {
       name: 'facebook',
       url: 'https://www.facebook.com/aumc.services',
-      icon: 'bi bi-facebook'
+      icon: 'bi bi-facebook',
     },
     {
       name: 'instagram',
       url: 'https://www.instagram.com/aumcservices.oficial/',
-      icon: 'bi bi-instagram'
+      icon: 'bi bi-instagram',
     },
     {
       name: 'whatsapp',
       url: 'https://api.whatsapp.com/send?phone=+522284208734&text=Hola,%20me%20gustar%C3%ADa%20saber%20m%C3%A1s%20sobre%20sus%20servicios',
-      icon: 'bi bi-whatsapp'
+      icon: 'bi bi-whatsapp',
     },
-  ]
-
+    {
+      name: 'correo',
+      url: 'mailto:contacto@aumcservices.com',
+      icon: 'bi bi-envelope',
+    },
+  ];
 }
