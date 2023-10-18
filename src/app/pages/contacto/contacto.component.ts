10a09f4a970eed0875af06e5aae97b05d0bcf717
dualipin <118947989+dualipin@users.noreply.github.com>
2023-10-17 22:37:48 -0600

    feat: contact section

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-
-
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { SocialNetworkService } from 'app/services/social-network.service';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contacto.component.html',
-  styleUrls: ['./contacto.component.css']
+  styleUrls: ['./contacto.component.css'],
 })
-export class ContactoComponent  {
+export class ContactoComponent {
 
-  constructor() { }
+  formContacto = new FormGroup({
+    nombre: new FormControl('', { validators: Validators.required }),
+    email: new FormControl('', Validators.required),
+    phone: new FormControl('', Validators.required),
+    mensaje: new FormControl('', Validators.required),
+  });
 
- 
+  constructor(private sn: SocialNetworkService) {}
+
+  socialNetworks = this.sn.getSocialNetworks();
+
+  isActiveModal = false
+
+  formSubmit() {
+    this.isActiveModal = true
+  }
+
+  deactivateModal(){
+    this.isActiveModal = false
+  }
 }
