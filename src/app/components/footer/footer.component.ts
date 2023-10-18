86d96810b51673c860662dc69dd1916587460f49
dualipin <118947989+dualipin@users.noreply.github.com>
2023-10-17 22:47:35 -0600

    feat: add service of social networks

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,36 +1,14 @@
 import { Component } from '@angular/core';
+import SocialNetwork from 'app/interfaces/ISocialNetwork';
+import { SocialNetworkService } from 'app/services/social-network.service';
 
-interface SocialNetwork {
-  name: string;
-  url: string;
-  icon?: string;
-}
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
-  socialNetworks: SocialNetwork[] = [
-    {
-      name: 'facebook',
-      url: 'https://www.facebook.com/aumc.services',
-      icon: 'bi bi-facebook',
-    },
-    {
-      name: 'instagram',
-      url: 'https://www.instagram.com/aumcservices.oficial/',
-      icon: 'bi bi-instagram',
-    },
-    {
-      name: 'whatsapp',
-      url: 'https://api.whatsapp.com/send?phone=+522284208734&text=Hola,%20me%20gustar%C3%ADa%20saber%20m%C3%A1s%20sobre%20sus%20servicios',
-      icon: 'bi bi-whatsapp',
-    },
-    {
-      name: 'correo',
-      url: 'mailto:contacto@aumcservices.com',
-      icon: 'bi bi-envelope',
-    },
-  ];
+  constructor(public sn: SocialNetworkService) {}
+
+  socialNetworks: SocialNetwork[] = this.sn.getSocialNetworks();
 }
