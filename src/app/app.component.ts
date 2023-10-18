79b97a2731c6ce6117adc24327f5b4afeb3ae983
dualipin <118947989+dualipin@users.noreply.github.com>
2023-10-17 22:51:15 -0600

    style: refactor code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,13 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     Aos.init()
   }
-  title = "page-aumc"
+
+  title = "AUMC Services"
 
   onDeactivate() {
     window.scrollTo(0, 0)
     // Alternatively, you can scroll to top by using this other call:
     // window.scrollTo(0, 0)
   }
-
-  prepareRoute(outlet: RouterOutlet) {
-    return (
-      outlet &&
-      outlet.activatedRouteData &&
-      outlet.activatedRouteData["animation"]
-    )
-  }
+  
 }
